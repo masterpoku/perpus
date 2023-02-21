46f7c8b7efa6c9f22f3ee5d4d3ab68a68e93621d
masterpoku <wafitakputusasa@gmail.com>
2023-02-21 09:24:14 +0700

    Update buku.php

--- admin/buku.php
+++ admin/buku.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - CODE: " . $row["code"]. " " . $row["jdl_buku"]. " " . $row["updt_buku"]. " " . $row["status"] . " " . $row["img"]. "<br>";
+    echo "id: " . $row["id"]. " - CODE: " . $row["code"]. " " . $row["jdl_buku"]. " " . $row["create"]. " " . $row["status"] . " " . $row["img"]. "<br>";
   }
 } else {
   echo "0 results";
