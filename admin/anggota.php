028d7770a09cd8c918d67130f17fcad04fc0e785
masterpoku <wafitakputusasa@gmail.com>
2023-02-21 09:22:57 +0700

    Update anggota.php

--- admin/anggota.php
+++ admin/anggota.php
@@ -1,6 +1,15 @@
 <?php
 
 include '../koneksi.php';
+
+
+$sql = "SELECT COUNT(*) FROM `mahasiswa` WHERE `status` = 0 ORDER BY `id` DESC;";
+$result = $conn->query($sql);
+$rows = $result->fetch_assoc();
+echo "TOTAL ANGGOTA BELUM AKTIF: " . $rows["COUNT(*)"]. "  <br><br><br><br>";
+
+
+
 // $username  = $_REQUEST['username'];
 $sql = "SELECT * FROM `mahasiswa` ORDER BY `mahasiswa`.`id` DESC";
 $result = $conn->query($sql);
@@ -8,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - REG: " . $row["req_perpus"]. " " . $row["nama"]. " " . $row["STB"]. " " . $row["fklts"]. " " . $row["jrsn"]. " " . $row["almt"]. "  " . $row["email"]. " " . $row["username"]. " " . $row["password"]. " " . $row["level"]. " " . $row["status"]. " " . $row["img"]. " " . $row["create"]. " <a href=anggota.php?active=". $row["id"].">link text</a> <br>";
+    echo "id: " . $row["id"]. " - REG: " . $row["req_perpus"]. " " . $row["nama"]. " " . $row["STB"]. " " . $row["fklts"]. " " . $row["jrsn"]. " " . $row["almt"]. "  " . $row["email"]. " " . $row["username"]. " " . $row["password"]. " " . $row["level"]. " " . $row["status"]. " " . $row["img"]. " " . $row["created"]. " <a href=anggota.php?active=". $row["id"].">link text</a> <br>";
   }
 } else {
   echo "0 results";
@@ -29,4 +38,13 @@ if ($conn->query($sql) === TRUE) {
 
 
 }
+
+
+
+
+
+
+
+
+
 ?>
